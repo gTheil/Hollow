1476db4ddf516aa3898e41d50674c625b684652d
Felipe Theil <lipetheil18@gmail.com>
2018-04-17 21:24:58 -0300

    Criado método específico para o pulo do personagem. Pulo e pulo duplo refeitos.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,7 +12,7 @@ public class Player : MonoBehaviour {
 	private float speed; // Velocidade atual do personagem
 	private bool facingRight = true; // Determina a direção em que o personagem está virado; ao inicializar, o personagem estará virado para a direita
 	private bool onGround; // Determina se o personagem está no chão ou no ar
-	private bool jump = false; // Determina se o personagem está num estado de pulo
+	private bool jump; // Determina se o personagem está em estado de pulo
 
 	// Inicialização
 	void Start () {
@@ -25,14 +25,21 @@ public class Player : MonoBehaviour {
 		onGround = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground")); // Dispara uma linha da posição atual do personagem até a posição do chão
 
 		// Se o botão de pulo for pressionado enquanto o personagem estiver no chão, ele estará em estado de pulo
-		if (Input.GetButtonDown ("Jump") && (onGround))
+		if (Input.GetButtonDown ("Jump") && (onGround)) {
+			Jump ();
 			jump = true;
+		}
+		// Se o botão de pulo for pressionado no ar durante o estado de pulo, o personagem realizará um segundo pulo, e depois será impedido de realizar pulos subsequentes
+		if (Input.GetButtonDown ("Jump") && (!onGround) && jump == true) {
+			Jump ();
+			jump = false;
+		}
 	}
 
 	// Atualiza em um intervalo específico
 	void FixedUpdate () {
 		float h = Input.GetAxisRaw("Horizontal"); // Variável que controla a posição X (horizontal) do personagem
-		rb.velocity = new Vector2(h * speed, rb.velocity.y); // A velocidade do RigidBody na horizontal é função da posição do personagem e sua velocidade atual
+		rb.velocity = new Vector2(h * speed, rb.velocity.y); // Variável que controla o movimento na horizontal e vertical do personagem
 
 		// Se a posição horizontal for maior que 0, ou seja, o personagem estiver se movendo para a direita, a função Flip é chamada para virar a imagem do personagem
 		if (h > 0 && !facingRight) {
@@ -43,20 +50,20 @@ public class Player : MonoBehaviour {
 		else if (h < 0 && facingRight) {
 			Flip();
 		}
-
-		// Ao personagem pular, sua velocidade é zerada e uma força vertical é aplica a seu RigidBody, logo em seguida acaba-se o estado de pulo
-		if (jump) {
-			rb.velocity = Vector2.zero;
-			rb.AddForce(Vector2.up * jumpForce);
-			jump = false;
-		}
 	}
 
-	// Chamada ao personagem se virar da direita para a esquerda ou vice-versa
+	// Efetua o pulo do personagem
+	void Jump () {
+		rb.velocity = Vector2.zero; // Zera a velocidade do personagem
+		rb.AddForce(Vector2.up * jumpForce); // Aplica uma força vertical ao personagem
+	}
+
+	// Efetua a inversão da imagem do personagem
 	void Flip () {
 		facingRight = !facingRight; // Inverte o valor de facingRight, fazendo o personagem virar a esquerda se estiver virado a direita e vice-versa
 		Vector3 scale = transform.localScale; // Recebe o valor da escala do personagem
 		scale.x *= -1; // Inverte o valor x (horizontal) da escala entre valores positivos e negativos
 		transform.localScale = scale; // Atualiza o valor da escala do personagem com o novo valor positivo ou negativo pra virar sua imagem
 	}
+
 }
